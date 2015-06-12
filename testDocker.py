e6848783edb2427d8e7626ba880df13d5dcf426b
ZephyrYin <dieinthewind@gmail.com>
2015-06-11 18:49:18 -0700

    Update testDocker.py

--- testDocker.py
+++ testDocker.py
@@ -1,19 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
 import docker
-import time
-c = docker.Client(base_url = 'unix://var/run/docker.sock')
-c.images(name = 'busybox')
-busyBox = []
-for i in range(3):
-	print i
-	busyBox.append(c.create_container(image = 'busybox', command='echo this is container ' + str(i)))
-	c.start(busyBox[-1].get('Id'))
-	print busyBox[-1].get('Id')
-	print c.logs(busyBox[-1].get('Id'))
+import os
 
-time.sleep(2)
+VERSION = 1.9
+TIMEOUT = 10
+TESTS_CONFIG_NAME = 'tests.config'
 
-for b in busyBox:
-	print 'stop'
-	print b.get('Id')
-	c.stop(b.get('Id'))
+def readConfig():
+    with open(TESTS_CONFIG_NAME) as file:
+        lines = file.readlines()
+        imageName = lines[0].rstrip()
+        tests = [line.rstrip() for line in lines[1:]]
+        return imageName, tests
 
+#imageName, tests = readConfig()
+imageName = 'ubuntu:12.04'
+tests = ['test1.py', 'test2.py']
+cli = docker.Client(base_url='unix://var/run/docker.sock', version=str(VERSION), timeout=TIMEOUT)
+cli.images(name = imageName)
+containers = []
+curPath = os.getcwd()
+for test in tests:
+	print test
+        containers.append(cli.create_container(image = imageName, command =  'ls'))
+	ID = containers[-1].get('Id')
+	print ID
+        os.system('sudo cp ' + test + ' /var/lib/docker/aufs/mnt/'+ ID+'/'+test)
+        cli.start(ID)
+        print cli.logs(ID)
