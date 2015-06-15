df8339fdc0d4b76477c4a4ff640249c0bf64b9ea
ZephyrYin <dieinthewind@gmail.com>
2015-06-15 11:11:01 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,1 +1,7 @@
-print 'this is test 2!'
+import os
+path = os.getcwd()
+with open('/mnt/testVol/test2Output.txt','w') as file:
+	file.write(path+'\n')
+	for i in range(20):
+		file.write(str(i) + '\n')
+		print 'this is test 1 !'
