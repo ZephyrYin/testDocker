c47659069b9062799a48625cae9e885efdc58ce3
ZephyrYin <dieinthewind@gmail.com>
2015-06-15 12:00:53 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,4 +4,4 @@ with open('/mnt/testVol/test2Output.txt','w') as file:
 	file.write(path+'\n')
 	for i in range(20):
 		file.write(str(i) + '\n')
-		print 'this is test 1 !'
+		print 'this is test 2 !'
