ccf9833520e0ff87d0fd97e8ef163058f0a180ef
ZephyrYin <dieinthewind@gmail.com>
2015-06-15 11:10:29 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,8 @@
-print 'this is test 1 !'
+import os
+path = os.getcwd();
+with open('/mnt/testVol/test1Output.txt','w') as file:
+	file.write(path+'\n')
+	for i in range(20):
+		file.write(str(i) + '\n')
+		print 'this is test 1 !'
+
